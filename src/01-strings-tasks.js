b8796c15bb002b7f2a4fde0f1a9766b1b407563b
tihonhomyshen <lilo.frich@gmail.com>
2024-01-12 13:40:16 +0300

    01 getRectangleString()

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -202,8 +202,28 @@ function extractEmails(str) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  // return 'asda';
+function getRectangleString(width, height) {
+  let string = '';
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (i === 0) {
+        if (j === 0) string += '┌';
+        else if (j === width - 1) string += '┐';
+        else { string += '─' }
+      }
+      else if (i === height - 1) {
+        if (j === 0) string += '└';
+        else if (j === width - 1) string += '┘';
+        else {string += '─'};
+      }
+      else {
+        if (j === 0 || j === width - 1) string += '│';
+        else string += ' ';
+      }
+    }
+    string += "\n";
+  }
+  return string;
 }
 
 
@@ -223,8 +243,9 @@ function getRectangleString(/* width, height */) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabetUpper = alphabet.toUpperCase;
 }
 
 /**
@@ -269,8 +290,12 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const deck = ['A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+  'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+  'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'];
+  return deck.indexOf(value);
 }
 
 
